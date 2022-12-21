0b3ad1ffdb41278db8348a9c918746870103cbef
a.zubchyk <Sanch13sanch@gmail.com>
2022-12-21 11:56:03 +0300

    Edited func check_sub_channel in middleware.py

--- logic_server/middleware.py
+++ logic_server/middleware.py
@@ -2,5 +2,5 @@ from aiogram.types import ChatMember
 
 
 def check_sub_channel(chat_member: ChatMember) -> bool:
-    """Checks if there is a user in the group. If there is a return True otherwise False"""
-    return True if chat_member['status'] != 'left' else False
+    """Return True otherwise False"""
+    return chat_member['status'] != 'left'
