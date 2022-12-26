410fd8247494ee1b451199866e8f7740412fb2f2
a.zubchyk <Sanch13sanch@gmail.com>
2022-12-26 10:56:52 +0300

    Добавил функции пересылки фото, получения всех команд бота.

--- handlers/handler.py
+++ handlers/handler.py
@@ -1,5 +1,5 @@
 from settings import config
-from logic_server.middleware import check_sub_channel
+from logic_server.middleware import check_sub_channel, get_all_bot_commands
 from aiogram import Dispatcher, Bot, types
 
 
@@ -14,26 +14,43 @@ async def start_handler(message: types.Message):
     if check_sub_channel(await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=user_id)):
         text = f"Hello, {user_full_name or ''}.\nYou pushed the start button\n"
         await message.answer(text=text)
+        await message.delete()
     else:
-        text = f"Вы не подписаны на группу {config.CHANNEL_LINK}. Подпишитесь!!!"
+        text = config.ANSWER_NOT_SUB
         await message.answer(text=text)
+        await message.delete()
 
 
 @dispatcher.message_handler(commands=config.COMMANDS['HELP'])
 async def help_handler(message: types.Message):
     user_full_name = message.from_user.full_name
-    text = f'Hi, {user_full_name}.\nYou pushed the help button'
+    text = f"Привет, {user_full_name or ''}.\n" \
+           f"Этот бот предназначен для отправки изображений в группу {config.CHANNEL_LINK}. \n" \
+           f"Подпишись на группу и отправляй изображения!"
     await message.answer(text=text)
+    await message.delete()
 
 
 @dispatcher.message_handler(content_types=['photo'])
 async def send_photo_to_group(message: types.Message):
-    image_id = message.photo[-1].file_id
-    await bot.send_photo(chat_id=config.CHANNEL_ID, photo=image_id)
+    user_id = message.from_user.id
+    if check_sub_channel(await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=user_id)):
+        image_id = message.photo[-1].file_id
+        await bot.send_photo(chat_id=config.CHANNEL_ID, photo=image_id)
+    else:
+        text = config.ANSWER_NOT_SUB
+        await message.answer(text=text)
+        await message.delete()
 
 
 @dispatcher.message_handler()
 async def reply_just_text(message: types.Message):
-    commands = config.COMMANDS.values()
-    text = f'Вы можете отправить только команды /{commands} или картинку'
-    await message.answer(text=text)
+    user_id = message.from_user.id
+    if check_sub_channel(await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=user_id)):
+        text = f"Вы можете отправлять только изображения и команды :" \
+               + get_all_bot_commands(commands=config.COMMANDS)
+        await message.answer(text=text)
+    else:
+        text = config.ANSWER_NOT_SUB
+        await message.answer(text=text)
+        await message.delete()
